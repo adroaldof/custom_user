4f002472d38b9f2386823b94037fecdf3361cdca
Adroaldo de Andrade <adroaldof@gmail.com>
2014-05-08 14:52:34 -0300

    Add models, admin and forms to accounts app
    Now is possible to create a new user in admin site

---
+++ accounts/forms.py
@@ -0,0 +1,125 @@
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.forms import TextInput
+from django.utils.translation import ugettext_lazy as _
+
+from .models import CustomUser
+
+
+class CustomUserCreationForm(forms.ModelForm):
+    """
+    A form that creates a user, with no privileges, from the given username and
+    password.
+    """
+    error_messages = {
+        'duplicate_useremail': _('A user with that email already exists'),
+        'password_mismatch': _("The two password fields didn't match"),
+    }
+    email = forms.EmailField(
+        label=_('Email address'),
+        widget=forms.TextInput(
+            attrs={'class': 'form-control input-md'}
+        ),
+        help_text=_('This email will be you username'),
+    )
+    password1 = forms.CharField(
+        label=_('Password'),
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control input-md'}
+        ),
+        help_text=_('Strong password is recommended'),
+    )
+    password2 = forms.CharField(
+        label=_('Password again'),
+        widget=forms.PasswordInput(
+            attrs={'class': 'form-control input-md'}
+        ),
+        help_text=_('Enter the same password as above, for verification')
+    )
+
+    class Meta:
+        # Point to our CustomUser here instead of default `User`
+        model = CustomUser
+        include = ('password1', 'password2')
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        try:
+            CustomUser._default_manager.get(email=email)
+        except CustomUser.DoesNotExist:
+            return email
+        raise forms.ValidationError(
+            self.error_messages['duplicate_useremail'],
+            code='duplicate_useremail',
+        )
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'])
+        return password2
+
+    def save(self, commit=True):
+        # Make sure we pass back in our CustomUserCreationForm and not the
+        # default `UserCreationForm`
+        user = super(CustomUserCreationForm, self).save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class CustomUserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField(
+        label=_("Password"),
+        help_text=_(
+            "Raw passwords are not stored, so there is no way to see "
+            "this user's password, but you can change the password "
+            "using <a href=\"password/\">this form</a>."
+        )
+    )
+
+    class Meta:
+        # Point to our CustomUser here instead of default `User`
+        model = CustomUser
+
+    def __init__(self, *args, **kwargs):
+        # Make sure we pass back in our CustomUserChangeForm and not the
+        # default `UserChangeForm`
+        super(CustomUserChangeForm, self).__init__(*args, **kwargs)
+        f = self.fields.get('user_permissions', None)
+        if f is not None:
+            f.queryset = f.queryset.select_related('content_type')
+
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial['password']
+
+
+class CustomUserUpdateForm(forms.ModelForm):
+    class Meta:
+        # Point to our CustomUser here instead of default `User`
+        model = CustomUser
+        fields = (
+            'full_name', 'short_name', 'email', 'email2', 'phone_cell',
+            'phone_home'
+        )
+        widgets = {
+            'full_name': TextInput(attrs={'class': 'form-control input-md'}),
+            'short_name': TextInput(attrs={'class': 'form-control input-md'}),
+            'email': TextInput(attrs={'class': 'form-control input-md'}),
+            'email2': TextInput(attrs={'class': 'form-control input-md'}),
+            'phone_cell': TextInput(attrs={'class': 'form-control input-md'}),
+            'phone_home': TextInput(attrs={'class': 'form-control input-md'}),
+            # 'date_of_birth': TextInput(
+            #     attrs={'class': 'form-control input-md'}
+            # ),
+        }
+        exclude = ('password', 'date_joined', 'last_login')
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,117 @@
-from django.db import models
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser, PermissionsMixin
+)
+from django.core.mail import send_mail
+from django.db import models
+from django.utils import timezone
+from django.utils.http import urlquote
+from django.utils.translation import ugettext_lazy as _
+
+
+class CustomUserManager(BaseUserManager):
+    def _create_user(
+        self, email, password, is_staff, is_superuser, **extra_fields
+    ):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        now = timezone.now()
+
+        if not email:
+            raise ValueError(_('Users must have an email address'))
+
+        email = self.normalize_email(email)
+        user = self.model(
+            email=email,
+            is_staff=is_staff,
+            is_active=True,
+            is_superuser=is_superuser,
+            last_login=now,
+            date_joined=now,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        return self._create_user(email, password, False, False, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        return self._create_user(email, password, True, True, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    full_name = models.CharField(_('full name'), max_length=254, blank=True)
+    short_name = models.CharField(
+        _('short name'),
+        max_length=30,
+        blank=True,
+        help_text=_('A nick name or how you would like to be called')
+    )
+    email = models.EmailField(
+        _('User email'),
+        max_length=254,
+        unique=True,
+        db_index=True,
+        help_text=_('This email will be used as your username')
+    )
+    email2 = models.EmailField(_('Help email'), max_length=254, blank=True)
+    phone_cell = models.CharField(_("cell phone"), max_length=100, blank=True)
+    phone_home = models.CharField(_("home phone"), max_length=100, blank=True)
+    date_of_birth = models.DateField(blank=True, null=True)
+    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
+    is_active = models.BooleanField(
+        _('active'),
+        default=True,
+        help_text=_(
+            'Designates whether this user should be treated as '
+            'is_active. Unselect this instead of deleting accounts.'
+        )
+    )
+    is_staff = models.BooleanField(
+        _('staff status'),
+        default=False,
+        help_text=_(
+            'Designates whether the user can log into this admin site.'
+        )
+    )
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
+
+    def __unicode__(self):
+        return self.email
+
+    def get_username(self):
+        return self.email
+
+    def get_absolute_url(self):
+        return "/users/%s/" % urlquote(self.email)
+
+    def get_full_name(self):
+        """
+        Returns the first_name plus the last_name, with a space in between.
+        """
+        full_name = self.full_name
+        if not full_name:
+            return self.email.strip()
+        return full_name.strip()
+
+    def get_short_name(self):
+        "Returns the short name for the user."
+        return self.short_name.strip()
+
+    def email_user(self, subject, message, from_email=None):
+        """
+        Sends an email to this User.
+        """
+        send_mail(subject, message, from_email, [self.email])

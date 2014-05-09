cb3ea601f3eb59631ff47c7d3c83c1149d11ddc1
Adroaldo de Andrade <adroaldof@gmail.com>
2014-05-09 14:53:37 -0300

    Add account settings view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.template.response import TemplateResponse
 
 # Create your views here.
+
+
+@login_required
+def account_settings(request):
+    return TemplateResponse(request, 'accounts/settings.html', {
+        'user': request.user,
+    })

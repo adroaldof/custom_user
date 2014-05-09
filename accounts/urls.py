37ce5756d25bf1fb2297ef1b02b27fa9460f270f
Adroaldo de Andrade <adroaldof@gmail.com>
2014-05-09 18:31:26 -0300

    Add login and logout, both using Django defaults
    Login was filled with the authentication form
    and template name to show a nice login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
+from django.contrib.auth import views as auth_views
 
+from .forms import CustomAuthenticationForm
 
 # Settings routes
 urlpatterns = patterns(
@@ -8,5 +10,24 @@ urlpatterns = patterns(
         r'^settings/',
         'account_settings',
         name='account-settings'
+    ),
+)
+
+urlpatterns += patterns(
+    '',
+    url(
+        r'^login/$',
+        auth_views.login,
+        {
+            'authentication_form': CustomAuthenticationForm,
+            'template_name': 'registration/login.html'
+        },
+        name='login'
+    ),
+    url(
+        r'^logout/$',
+        auth_views.logout,
+        {'template_name': 'registration/loged_out.html'},
+        name='logout'
     ),
 )

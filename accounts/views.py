f64710f786f9e21e136a181ef1e886817a214b0f
Adroaldo de Andrade <adroaldof@gmail.com>
2014-05-09 18:33:45 -0300

    Add custom user update class on view

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.template.response import TemplateResponse
+from django.core.urlresolvers import reverse
+from django.views.generic import UpdateView
 
-# Create your views here.
+from .forms import CustomUserUpdateForm
+from .models import CustomUser
 
 
 @login_required
@@ -10,3 +13,12 @@ def account_settings(request):
     return TemplateResponse(request, 'accounts/settings.html', {
         'user': request.user,
     })
+
+
+class CustomUserUpdateView(UpdateView):
+    model = CustomUser
+    form_class = CustomUserUpdateForm
+    template_name = 'accounts/settings.html'
+
+    def get_success_url(self):
+        return reverse('account-settings', args=[self.object.id, ])

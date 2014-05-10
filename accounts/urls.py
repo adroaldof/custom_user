5f6fc748df8689ef082b94e4c85017d0612d42e1
Adroaldo de Andrade <adroaldof@gmail.com>
2014-05-10 11:57:11 -0300

    Move login and logout files to registration directory

--- accounts/urls.py
+++ accounts/urls.py
@@ -54,13 +54,13 @@ urlpatterns += patterns(
     url(
         r'^password/change$',
         auth_views.password_change,
-        {'template_name': 'accounts/password_change_form.html'},
+        {'template_name': 'registration/password_change_form.html'},
         name='password_change'
     ),
     url(
         r'^password/change/done$',
         auth_views.password_change_done,
-        {'template_name': 'accounts/password_change_done.html'},
+        {'template_name': 'registration/password_change_done.html'},
         name='password_change_done'
     ),
 )
